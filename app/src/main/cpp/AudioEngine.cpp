3f910e590961d82794c93538d2ea5e061199c4c2
olciak <aleksandramaslankiewicz0@gmail.com>
2021-03-04 20:42:04 +0100

    recording fix ale nie wszystko jest fixed :(

--- app/src/main/cpp/AudioEngine.cpp
+++ app/src/main/cpp/AudioEngine.cpp
@@ -12,12 +12,35 @@ AudioEngine::~AudioEngine() {
 }
 
 void AudioEngine::StartRecording(){
-    OpenRecordingStream();
-    if (mRecordingStream) {
-        StartStream(mRecordingStream);
-    } else {
-        CloseStream(mRecordingStream);
-    }
+    AudioStreamBuilder builder;
+    builder.setDirection(Direction::Input);
+    builder.setPerformanceMode(PerformanceMode::LowLatency);
+
+    AudioStream *stream;
+    Result r = builder.openStream(&stream);
+    r = stream->requestStart();
+    constexpr  int kMilis = 2;
+    const int32_t frames = (int32_t)(kMilis*(stream->getSampleRate()/kMillisPerSecond));
+    int32_t  buffer[frames];
+    constexpr int64_t timeOut = 3*kNanosPerMillisecond;
+    int framesRead = 0;
+    do{
+        auto result = stream->read(buffer,stream->getBufferSizeInFrames(),0);
+        if(result != Result::OK) break;
+        framesRead = result.value();
+    }while(framesRead != 0);
+
+
+    auto result = stream->read(buffer,frames,timeOut);
+if(result ==Result::OK){
+    double x = result.value();
+}
+//    OpenRecordingStream();
+//    if (mRecordingStream) {
+//        StartStream(mRecordingStream);
+//    } else {
+//        CloseStream(mRecordingStream);
+//    }
 }
 
 void AudioEngine::StopRecording() {
